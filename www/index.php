11c259f2e40a1dfb765acfbbbb876df6d51711ca
sdeix <you@example.com>
2024-10-31 22:01:43 +0700

    Controller в MVC

--- www/index.php
+++ www/index.php
@@ -1,12 +1,15 @@
 <?php
 
-function myAutoLoader(string $className)
-{
+spl_autoload_register(function (string $className) {
     require_once __DIR__ . '/../src/' . str_replace('\\', '/', $className) . '.php';
+});
+
+$controller = new \MyProject\Controllers\MainController();
+
+
+if (!empty($_GET['name'])) {
+    $controller->sayHello($_GET['name']);
+} else {
+    $controller->main();
 }
 
-spl_autoload_register('myAutoLoader');
-
-$author = new \MyProject\Models\Users\User('Иван');
-$article = new \MyProject\Models\Articles\Article('Заголовок', 'Текст', $author);
-var_dump($article);
